8e39cd1be2649261ecbadaded86a0d7d5eaef9af
Nithupm <nithu.zugzwang@gmail.com>
2022-06-26 22:42:28 +0530

    updates

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {FaBars} from "react-icons/fa"
 import {FaRegHeart} from "react-icons/fa"
 import {FaShoppingCart} from "react-icons/fa"
+import {FaSearch} from "react-icons/fa"
 import '../Components/Navbar.css'
 
 function Navbar() {
@@ -11,9 +12,17 @@ function Navbar() {
     <div className="bars">
     <FaBars size={"30px"} />
     </div>
+    <div className="search">
+      <input type="text" className="find" placeholder='search for products brands and more'/>
+      <div className="find-icon">
+      <FaSearch/>
+      </div>
+    </div>
+
     <div className="logo">
     <img src="capricon.png.png" alt="" className="img" />
     </div>
+   
     <div className="heart">
    
     <FaRegHeart size={"30px"}/>
